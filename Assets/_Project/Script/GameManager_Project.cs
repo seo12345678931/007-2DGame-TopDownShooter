b46c8c3c66bec9379a3cd51eb107184d0d1e54ef
SeoSeungHwan <seo12345678931@gmail.com>
2026-02-16 02:09:26 +0900

    26.02.16 - 게임 매니저 개선 및 애니메이션 디테일 추가
    자체 스트라이트로 교체, 플레이어 UI 연동화 완료(체력, 무기선택), 달리기 모션 추가(블렌드 트리), 무기별 모션추가

---
+++ Assets/_Project/Script/GameManager_Project.cs
@@ -0,0 +1,71 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using _2DTopDown;
+
+namespace _2DTopDown
+{
+    public class GameManager_Project : MonoBehaviour
+    {
+        [Header("게임 UI")]
+        public TextMeshProUGUI TimerTxt;
+        public TextMeshProUGUI ScoreTxt;
+        public TextMeshProUGUI WeaponNameTxt;
+        public Image WeaponIcon;
+        public Sprite[] WeaponIcons;
+
+        [Header("무기 선택 토글")]
+        public Toggle[] WeaponSelectToggles;
+
+        [Header("체력 0에 도달 시 게임오버 설정")]
+        public bool gameOver = false;
+
+        // 싱글톤 인스턴스 설정
+        public static GameManager_Project instance;
+        void Awake()
+        {
+            // 싱글톤 초기화
+            if (instance == null) instance = this;
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            TimerTxt.text = TimeSpan.FromSeconds(Time.timeSinceLevelLoad).ToString("mm\\:ss");
+        }
+
+        public void SelectWeapon(Player.WeaponTypes weaponType)
+        {
+            switch (weaponType)
+            {
+                case Player.WeaponTypes.Knife:
+                    WeaponNameTxt.text = "Combat Knife";
+                    WeaponIcon.sprite = WeaponIcons[0];
+                    SetToggleState(0);
+                    break;
+                case Player.WeaponTypes.Pistol:
+                    WeaponNameTxt.text = "1911 Pistol";
+                    WeaponIcon.sprite = WeaponIcons[1];
+                    SetToggleState(1);
+                    break;
+            }
+        }
+
+        // 반복되는 코드를 줄이기 위한 헬퍼 함수입니다.
+        private void SetToggleState(int index)
+        {
+            for (int i = 0; i < WeaponSelectToggles.Length; i++)
+            {
+                // 인덱스가 맞으면 true, 아니면 false
+                WeaponSelectToggles[i].isOn = (i == index);
+            }
+        }
+    }
+}
--- Assets/_Project/Script/Player.cs
+++ Assets/_Project/Script/Player.cs
@@ -1,37 +1,119 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+namespace _2DTopDown
 {
-    Rigidbody rb;
-    public float moveSpeed = 10.0f;
-    public GameObject mousePointer;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
+    public class Player : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-    }
+        // 무기 타입
+        public enum WeaponTypes
+        {
+            Knife = 0,
+            Pistol = 1,
+            Null = 2,
+            Rifle = 3,
+            ShotGun = 4
+        }
+        WeaponTypes CurrWeapon = WeaponTypes.Null;
 
-    // 이동조작 및 움직임
-    private void FixedUpdate()
-    {
-        float H = Input.GetAxis("Horizontal");
-        float V = Input.GetAxis("Vertical");
-        Vector3 moveVec = new Vector3(H * moveSpeed, 0.0f, V * moveSpeed);
-        rb.linearVelocity = moveVec;
+        Rigidbody rb;
+        public float moveSpeed = 10.0f;
+        public GameObject mousePointer;
 
-        UpdateAim();
-    }
+        [Header("체력")]
+        public float maxHP;
+        public float currentHP;
+        public Image HealthBar;
+        public TextMeshProUGUI HealthNum;
 
-    // 마우스 포인터
-    public void UpdateAim()
-    {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.y = transform.position.y;
-        mousePointer.transform.position = mousePos;
-        float deltaY = mousePos.z - transform.position.z;
-        float deltaX = mousePos.x - transform.position.x;
-        float angleInDegrees = Mathf.Atan2(deltaY, deltaX) * 180 / Mathf.PI;
-        transform.eulerAngles = new Vector3(0, -angleInDegrees, 0);
+        [Header("애니메이션")]
+        public Animator Anim;
+        public Animator Anim_Leg;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            currentHP = maxHP;
+            HealthBar.fillAmount = currentHP / maxHP;
+            HealthNum.text = $"{currentHP:F0} / {maxHP:F0}";
+            SetWeapon(WeaponTypes.Knife);
+        }
+
+        private void Update()
+        {
+            //  이동조작 및 움직임
+            float H = Input.GetAxis("Horizontal");
+            float V = Input.GetAxis("Vertical");
+            Vector3 moveVec = new Vector3(H * moveSpeed, 0.0f, V * moveSpeed);
+            rb.linearVelocity = moveVec;
+            Anim_Leg.SetBool("isWalk", moveVec.magnitude > 0.1);
+            Anim_Leg.SetFloat("xDir", H);
+            Anim_Leg.SetFloat("yDir", V);
+
+            // 마우스 포인터 갱신
+            UpdateAim();
+
+            switch (CurrWeapon)
+            {
+                case WeaponTypes.Knife:
+                    if (Input.GetMouseButton(0))
+                    {
+
+                    }
+                    break;
+                case WeaponTypes.Pistol:
+                    if (Input.GetMouseButtonDown(0))
+                    {
+
+                    }
+                    break;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                SetWeapon(WeaponTypes.Knife);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                SetWeapon(WeaponTypes.Pistol);
+            }
+        }
+
+        // 마우스 포인터
+        public void UpdateAim()
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.y = transform.position.y;
+            mousePointer.transform.position = mousePos;
+            float deltaY = mousePos.z - transform.position.z;
+            float deltaX = mousePos.x - transform.position.x;
+            float angleInDegrees = Mathf.Atan2(deltaY, deltaX) * 180 / Mathf.PI;
+            transform.eulerAngles = new Vector3(0, -angleInDegrees, 0);
+        }
+
+        // 무기세팅
+        public void SetWeapon(WeaponTypes weaponType)
+        {
+            if (weaponType != CurrWeapon)
+            {
+                CurrWeapon = weaponType;
+                Anim.SetTrigger("WeaponChange");
+                switch (weaponType)
+                {
+                    case WeaponTypes.Knife:
+                        Anim.SetInteger("WeaponType", 0);
+                        break;
+                    case WeaponTypes.Pistol:
+                        Anim.SetInteger("WeaponType", 1);
+                        break;
+                }
+            }
+            if (GameManager_Project.instance != null)
+            {
+                GameManager_Project.instance.SelectWeapon(weaponType);
+            }
+        }
     }
 }
